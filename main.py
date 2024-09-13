c3273d1ed8eb2dbe16814066281fc6edb20d0567
Evan Li <hl490@duke.edu>
2024-09-13 14:34:49 +0000

    cicd done

--- main.py
+++ main.py
@@ -9,23 +9,30 @@ def calculate_stat(data):
     print(salaryDataDesc)
     return salaryDataDesc
 
+
 salaryData = pd.read_csv("Dataset-salary-2024.csv")
-columns_to_keep = ['work_year', 'experience_level', 'job_title',
-                   'salary_in_usd', 'remote_ratio', 'company_size']
+columns_to_keep = [
+    "work_year",
+    "experience_level",
+    "job_title",
+    "salary_in_usd",
+    "remote_ratio",
+    "company_size",
+]
 filteredData = salaryData[columns_to_keep]
 
 stats = calculate_stat(filteredData)
 
 # create a histogram of the salary data
 plt.figure(figsize=(10, 6))
-filteredData['salary_in_usd'].hist(bins=50)
-plt.title('Distribution of Salaries')
-plt.xlabel('Salary')
-plt.ylabel('Frequency')
+filteredData["salary_in_usd"].hist(bins=50)
+plt.title("Distribution of Salaries")
+plt.xlabel("Salary")
+plt.ylabel("Frequency")
 
 # save the plot to a buffer
 img_buffer = BytesIO()
-plt.savefig(img_buffer, format='png')
+plt.savefig(img_buffer, format="png")
 img_buffer.seek(0)
 
 # prepare the text content
